29128c5e6b6c877ffe34549f514536b6c04994b4
Dani Ocaranza <96756521+Dani-0TB@users.noreply.github.com>
2024-05-24 19:42:25 -0400

    terminado crud productos y categorias

--- api/productos/urls.py
+++ api/productos/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from .views import (ProductosRoot, CategoriaList)
+from .views import (ProductosRoot, 
+                    CategoriaList, CategoriaDetalle, 
+                    SubcategoriaList, SubcategoriaDetalle,
+                    ProductoList, ProductoDetalle)
 
 urlpatterns = [
     path('', ProductosRoot.as_view(), name='productos-root'),
-    path('categoriaList', CategoriaList.as_view(), name='categoriaList')
+    path('categoriaList', CategoriaList.as_view(), name='categoriaList'),
+    path('categoriaDetalle/<int:pk>', CategoriaDetalle.as_view()),
+    path('subcategoriaList/', SubcategoriaList.as_view()),
+    path('subcategoriaDetalle/<int:pk>', SubcategoriaDetalle.as_view()),
+    path('productoList/', ProductoList.as_view()),
+    path('productoDetalle/<int:pk>', ProductoDetalle.as_view())
 ]
