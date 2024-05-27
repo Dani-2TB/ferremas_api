31d8955ad32eb6ab2b180bbc3f69b680cfa2bdb3
Dani Ocaranza <96756521+Dani-0TB@users.noreply.github.com>
2024-05-27 01:02:53 -0400

    update endpoints productos

--- api/productos/urls.py
+++ api/productos/urls.py
@@ -1,15 +1,12 @@
 from django.urls import path
 from .views import (ProductosRoot, 
-                    CategoriaList, CategoriaDetalle, 
-                    SubcategoriaList, SubcategoriaDetalle,
+                    CategoriaList, CategoriaDetalle,
                     ProductoList, ProductoDetalle)
 
 urlpatterns = [
     path('', ProductosRoot.as_view(), name='productos-root'),
     path('categoriaList', CategoriaList.as_view(), name='categoriaList'),
     path('categoriaDetalle/<int:pk>', CategoriaDetalle.as_view()),
-    path('subcategoriaList/', SubcategoriaList.as_view()),
-    path('subcategoriaDetalle/<int:pk>', SubcategoriaDetalle.as_view()),
     path('productoList/', ProductoList.as_view()),
     path('productoDetalle/<int:pk>', ProductoDetalle.as_view())
 ]
